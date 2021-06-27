4d62a0dbcfcbd98b7dd59bb5a9df88f2964fcec4
hanjiyeon <jiyeon53885@gmail.com>
2021-06-27 14:51:07 +0900

    todo components

---
+++ src/components/Task.stories.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Task from "./Task";
+
+export default {
+  component: Task,
+  title: "Task",
+};
+
+const Template = (args) => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: "1",
+    title: "Test task",
+    state: "TASK_INBOX",
+    updateAt: new Date(2021, 5, 27, 14, 39),
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_PINNED",
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_ARCHIVED",
+  },
+};
